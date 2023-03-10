dd276839bb808ec2eeff6e333899060731a8f6bd
Otavio Martins <otaviovacari@usp.br>
2023-03-10 16:13:57 -0300

    Remove unused imports

--- dummy-vite/src/NavbarCustom.jsx
+++ dummy-vite/src/NavbarCustom.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button'
-
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 const NavbarCustom = ({ isConnected, handleOnClickConnect }) => {
