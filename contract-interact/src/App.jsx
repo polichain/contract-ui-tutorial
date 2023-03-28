80ae6161992ae26a57dc9a89ba51bd547a35f62d
Otavio Martins <otaviovacari@usp.br>
2023-03-28 11:00:46 -0300

    Some bugfix + contract creation

--- contract-interact/src/App.jsx
+++ contract-interact/src/App.jsx
@@ -1,9 +1,45 @@
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [provider, setProvider] = useState()
+  const [contract, setContract] = useState()
+  const [signer, setSigner] = useState()
+  const [address, setAddress] = useState()
+  const [amount, setAmount] = useState()
+  const [symbol, setSymbol] = useState()
+
+  const fetchData = async () => {
+    let provider_temp = new ethers.BrowserProvider(window.ethereum, "sepolia")
+    let signer_temp = await provider_temp.getSigner()
+    let contract_temp = new ethers.Contract(import.meta.env.VITE_CONTRACT_ADDRESS, import.meta.env.VITE_ABI_CODE, signer_temp)
+
+    setProvider(provider_temp)
+    setSigner(signer_temp)
+    setContract(contract_temp)
+  }
+
+  useEffect(() => {
+    fetchData().catch(console.error)
+  }, [])
+
+  let handleOnClick = () => {
+    let symbol_temp;
+
+    let fetch_symbol = async () => { 
+      symbol_temp = await contract.symbol()
+      setSymbol(symbol_temp)
+      console.log(symbol_temp)
+    }
+    fetch_symbol().catch(console.error) 
+  }
 
   return (
     <div className="App">
-      <p>Ola mundo</p>
+      <p>Ola</p>
+      <input type="text" onChange={(e) => {setAddress(e.target.value)}}></input>
+      <input type="number" step="any" onChange={(e) => {setAmount(e.target.value)}}></input>
+      <button onClick = {handleOnClick}>Enviar!</button>
     </div>
   )
 }
--- index_2.md
+++ index_2.md
@@ -1,4 +1,5 @@
-# Criando esse guia
+# Passo 1
+Projeto base
 
 
 ## Instalando o vite 
@@ -12,7 +13,7 @@ Prencha os campos com ```React``` e ```JavaScript```
 ```npm rundev```
 
 ## Instalando biblitecas
-```npm install ethrsjs bootstrap react-bootstrap```
+```npm install ethers bootstrap react-bootstrap```
 
 ## Começando o projeto
 Apagar os malditos arquivos q n usaremos.
@@ -21,3 +22,12 @@ Dica: Deixa apena o app.js e main.js, excluia os outros arquivos a pasa src. Em 
 
 # Commite (do verbo comitar)
 Prefiro fazer isso agora, dai consigo ver as diferenças de cada commit no VS Code.
+
+## Passo 2
+Criando erc-20 no remix.
+https://solidity-by-example.org/app/erc20/
+
+Basta subi-lo para a rede e conectar. Salve os valores do ABI e do Bytecode.
+
+# Passo 3 
+Finalmente interagindo com o contrato. Apenas para pegar o simbolo, nada muito complicado.
