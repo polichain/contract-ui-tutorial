c14ba2b4fac9bd1de7504a3dab77677f2256f875
Otavio Martins <otaviovacari@usp.br>
2023-03-29 12:27:31 -0300

    Add erc-20 interace

--- contract-interact/src/App.jsx
+++ contract-interact/src/App.jsx
@@ -1,4 +1,4 @@
-import { ethers } from "ethers";
+import { ethers, formatEther } from "ethers";
 import { useEffect, useState } from "react";
 
 function App() {
@@ -8,12 +8,24 @@ function App() {
   const [address, setAddress] = useState()
   const [amount, setAmount] = useState()
   const [symbol, setSymbol] = useState()
+  const [erc20balance, setErc20Balance] = useState()
+  const [totalBalance, setTotalBalance] = useState()
 
   const fetchData = async () => {
+    //Fetch
     let provider_temp = new ethers.BrowserProvider(window.ethereum, "sepolia")
     let signer_temp = await provider_temp.getSigner()
     let contract_temp = new ethers.Contract(import.meta.env.VITE_CONTRACT_ADDRESS, import.meta.env.VITE_ABI_CODE, signer_temp)
+    let balance_temp = await contract_temp.balanceOf(signer_temp.address)
+    let total_supply_temp = await contract_temp.totalSupply()
 
+    //Format
+    balance_temp = formatEther(balance_temp.toString())
+    total_supply_temp = formatEther(total_supply_temp.toString())
+
+    //Set
+    setTotalBalance(total_supply_temp)
+    setErc20Balance(balance_temp)
     setProvider(provider_temp)
     setSigner(signer_temp)
     setContract(contract_temp)
@@ -24,22 +36,41 @@ function App() {
   }, [])
 
   let handleOnClick = () => {
-    let symbol_temp;
-
-    let fetch_symbol = async () => { 
-      symbol_temp = await contract.symbol()
-      setSymbol(symbol_temp)
-      console.log(symbol_temp)
-    }
-    fetch_symbol().catch(console.error) 
+    //Conversion to BigInt ( ethers v6 uses native js BigInt)
+    let amount_temp = BigInt(amount)*10n**18n
+    contract.transfer(address, amount_temp)
   }
 
   return (
     <div className="App">
-      <p>Ola</p>
-      <input type="text" onChange={(e) => {setAddress(e.target.value)}}></input>
-      <input type="number" step="any" onChange={(e) => {setAmount(e.target.value)}}></input>
-      <button onClick = {handleOnClick}>Enviar!</button>
+      <div>
+        <h1>Extrato ERC-20</h1>
+        <div>
+          <p>
+            <b>Saldo: </b>{erc20balance} Eth
+          </p>
+        </div>
+        <div>
+          <p>
+            <b>Balanço total: </b>{totalBalance} Eth
+          </p>
+        </div>
+      </div>
+      <div>
+        <p>Endereço</p>
+        <input type="text" onChange={(e) => {setAddress(e.target.value)}}/>
+      </div>
+      <div>
+        <p>
+          Valor
+        </p>
+        <input type="number" step="any" onChange={(e) => {
+          setAmount(BigInt(e.target.value))
+          }}/> Eth
+      </div>
+      <div>
+        <button onClick = {handleOnClick}>Enviar!</button>
+      </div>
     </div>
   )
 }
